e2a9e96a822861c0e7e20a7c58d2117f72ed9537
chagansingh <chagansingh9876@gmail.com>
2023-02-16 07:41:12 +0530

    redirect added

--- src/Layout/Header/Navbar/NavbarHeader.js
+++ src/Layout/Header/Navbar/NavbarHeader.js
@@ -31,7 +31,8 @@ const isLoggedIn=AuthCtx.isLoggedIn;
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/products">Store</Nav.Link>
+            {isLoggedIn && <Nav.Link as={Link} to="/products">Store</Nav.Link>}
+            {!isLoggedIn && <Nav.Link as={Link} to="/login">Sotre</Nav.Link>}
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/contactus">Contact Us</Nav.Link>
             <Nav.Link as={Link} to="/login">LogIn</Nav.Link>
